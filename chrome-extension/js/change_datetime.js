fe1c7d745a07d8033397a03730418fbdf1bfa6e1
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-03-01 22:56:08 +0900

    [Add] 曜日表示を追加 (resolve #1)

--- chrome-extension/js/change_datetime.js
+++ chrome-extension/js/change_datetime.js
@@ -1,6 +1,7 @@
 {
 	"use strict";
 	const extension_id = chrome.runtime.id;
+	const dayOfWeeks = "日月火水木金土".split("");
 	window.setInterval(() => {
 		document.querySelectorAll(`[datetime]:not([data-${extension_id}-changed])`).forEach(elem => {
 			elem.setAttribute(`data-${extension_id}-changed`, true);
@@ -9,11 +10,13 @@
 			const yyyy = datetime.getFullYear();
 			const MM = datetime.getMonth() + 1;
 			const dd = datetime.getDate();
+			// 曜日
+			const E = dayOfWeeks[datetime.getDay()];
 			const HH = datetime.getHours();
 			const mm = datetime.getMinutes();
 			const ss = datetime.getSeconds();
 			const span = document.createElement("span");
-			span.appendChild(document.createTextNode(`${yyyy}/${MM}/${dd} ${HH}:${mm}:${ss}`));
+			span.appendChild(document.createTextNode(`${yyyy}/${MM}/${dd}(${E}) ${HH}:${mm}:${ss}`));
 			span.appendChild(document.createTextNode(" ("));
 			elem.parentNode.replaceChild(span, elem);
 			span.appendChild(elem);
