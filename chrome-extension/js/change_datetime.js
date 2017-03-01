868d628533767331d9e96467ee4cbd437a0fa3b5
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-03-01 22:53:35 +0900

    [Update] date formatを明示的に指定

--- chrome-extension/js/change_datetime.js
+++ chrome-extension/js/change_datetime.js
@@ -6,8 +6,14 @@
 			elem.setAttribute(`data-${extension_id}-changed`, true);
 			const datetime_str = elem.getAttribute("datetime");
 			const datetime = new Date(datetime_str);
+			const yyyy = datetime.getFullYear();
+			const MM = datetime.getMonth() + 1;
+			const dd = datetime.getDate();
+			const HH = datetime.getHours();
+			const mm = datetime.getMinutes();
+			const ss = datetime.getSeconds();
 			const span = document.createElement("span");
-			span.appendChild(document.createTextNode(datetime.toLocaleString()));
+			span.appendChild(document.createTextNode(`${yyyy}/${MM}/${dd} ${HH}:${mm}:${ss}`));
 			span.appendChild(document.createTextNode(" ("));
 			elem.parentNode.replaceChild(span, elem);
 			span.appendChild(elem);
